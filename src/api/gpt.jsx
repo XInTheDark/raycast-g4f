49f6757e12153b8d0d92abe0bd5aad88928eab89
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-04-30 12:44:47 +0800

    Add streaming support (#4)
    Uses the code given by the g4f-ts package. Currently only useful for the Bing provider.

--- src/api/gpt.jsx
+++ src/api/gpt.jsx
@@ -25,6 +25,7 @@ import Gemini from "gemini-ai";
 export const GeminiProvider = "GeminiProvider";
 
 import fs from "fs";
+import { chunkProcessor } from "g4f";
 
 export default (props, { context = undefined, allowPaste = false, useSelected = false, buffer = [] }) => {
   const Pages = {
@@ -58,15 +59,26 @@ export default (props, { context = undefined, allowPaste = false, useSelected = 
       // load provider and model from preferences
       const preferences = getPreferenceValues();
       const providerString = preferences["gptProvider"];
-      const [provider, model] = providers[providerString];
+      const [provider, model, stream] = providers[providerString];
       const options = {
         provider: provider,
         model: model,
+        stream: stream,
       };
 
       // generate response
-      let response = await chatCompletion(messages, options);
-      setMarkdown(response);
+      let response = "";
+      if (!stream) {
+        response = await chatCompletion(messages, options);
+        setMarkdown(response);
+      } else {
+        let r = await chatCompletion(messages, options);
+        for await (const chunk of chunkProcessor(r)) {
+          response += chunk;
+          response = formatResponse(response);
+          setMarkdown(response);
+        }
+      }
       setLastResponse(response);
 
       await showToast({
@@ -181,10 +193,10 @@ export default (props, { context = undefined, allowPaste = false, useSelected = 
 };
 
 export const providers = {
-  GPT4: [g4f.providers.GPT, "gpt-4-32k"],
-  GPT35: [g4f.providers.GPT, "gpt-3.5-turbo"],
-  Bing: [g4f.providers.Bing, "gpt-4"],
-  GoogleGemini: [GeminiProvider, "gemini-pro"],
+  GPT4: [g4f.providers.GPT, "gpt-4-32k", false],
+  GPT35: [g4f.providers.GPT, "gpt-3.5-turbo", false],
+  Bing: [g4f.providers.Bing, "gpt-4", true],
+  GoogleGemini: [GeminiProvider, "gemini-pro", true],
 };
 
 // generate response using a chat context and options
@@ -210,7 +222,11 @@ export const chatCompletion = async (chat, options) => {
   }
 
   // format response
-  response = formatResponse(response);
+  if (typeof response === "string") {
+    // will not be a string if stream is enabled
+    response = formatResponse(response);
+  }
+
   return response;
 };
 
@@ -233,10 +249,11 @@ export const getChatResponse = async (currentChat, query) => {
 
   // load provider and model
   const providerString = currentChat.provider;
-  const [provider, model] = providers[providerString];
+  const [provider, model, stream] = providers[providerString];
   const options = {
     provider: provider,
     model: model,
+    stream: stream,
   };
 
   // generate response
@@ -251,9 +268,9 @@ export const formatResponse = (response) => {
   response = response.replace(/\\t/g, "\t");
   response = response.replace(/\\r/g, "\r");
 
-  // convert <sup> and <sub>
-  response = response.replace(/<sup>(.*?)<\/sup>/g, " [$1]");
-  response = response.replace(/<sub>(.*?)<\/sub>/g, " [$1]");
+  // remove <sup>, </sup> tags (not supported apparently)
+  response = response.replace(/<sup>/g, "");
+  response = response.replace(/<\/sup>/g, "");
 
   return response;
 };
