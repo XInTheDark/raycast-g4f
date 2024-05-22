d452fae84d59873bed805e8cccdfd58e54c5b222
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-05-22 15:58:49 +0800

    improve web search new prompt, slightly enhanced formatting.

--- src/api/web.jsx
+++ src/api/web.jsx
@@ -9,11 +9,18 @@ export const webSystemPrompt =
   "output a single message, containing the word <|web_search|> (EXACTLY AS IT IS GIVEN TO YOU), followed by ONLY your web search query. " +
   "After outputting your search query, you MUST output the token <|end_web_search|> (EXACTLY AS IT IS GIVEN TO YOU) to denote the end of the search query. " +
   "Keep your search query concise as there is a 400 characters limit.\n" +
-  "The system will then append the web search results at the end of the user message, starting with the token <|web_search_results|>. You can use these search results in your response." +
-  " If the token <|web_search_results|> is present in the user's message, then you MUST NOT request another web search.\n" +
-  "IMPORTANT! You should ONLY choose to search the web if it is STRONGLY relevant to the user's query. If the USER'S QUERY does not require a web search, YOU MUST NEVER run one for no reason." +
-  " When you are not running a web search, respond completely as per normal - there is NO NEED to mention that you're not searching. \n" +
-  "You MUST NOT make up any information.\n";
+  "The system will then append the web search results at the end of the user message, starting with the token <|web_search_results|>. " +
+  "Take note that the user CANNOT ACCESS the content under <|web_search_results|> - YOU should incorporate these results into your response. " +
+  "Additionally, If the token <|web_search_results|> is present in the user's message, then you MUST NOT request another web search.\n\n" +
+  "IMPORTANT! You should ONLY choose to search the web if it is STRONGLY relevant to the user's query, i.e. in the following circumstances: " +
+  "1. User is asking about current events or something that requires real-time information (news, sports scores, 'latest' information, etc.)\n" +
+  "2. User is asking about some term you are unfamiliar with - you don't have a good idea of what it is, or if it's a little-known term.\n" +
+  "3. User explicitly asks you to browse or provide links to references.\n" +
+  "If the user's query does NOT require a web search, YOU MUST NEVER run one for no reason.\n\n" +
+  "When using web search results, you are encouraged to cite references where appropriate, using inline links in standard markdown format.\n" +
+  "You are also allowed to make use of web search results from previous messages, if they are STRONGLY RELEVANT to the current message.\n\n" +
+  "When you are not running a web search, respond completely as per normal - there is NO NEED to mention that you're not searching. \n" +
+  "Do your best to be informative. However, you MUST NOT make up any information. If you think you might not know something, search for it.\n";
 export const systemResponse = "Understood. I will strictly follow these instructions in this conversation.";
 
 export const getWebResult = async (query) => {
@@ -46,7 +53,8 @@ export const processWebResults = (results) => {
   let answer = "";
   for (let i = 0; i < results.length; i++) {
     let x = results[i];
-    answer += x["title"] + "\n" + x["url"] + "\n" + x["content"] + "\n\n";
+    let rst = `Title: ${x["title"]}\nURL: ${x["url"]}\n${x["content"]}\n\n`;
+    answer += rst;
   }
   return answer;
 };
