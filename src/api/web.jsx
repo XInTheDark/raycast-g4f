70a4e43c3c529851f8dbe9d41a4063e57eb5e3cc
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-05-25 14:46:56 +0800

    Minor improvements to Web Search prompting

--- src/api/web.jsx
+++ src/api/web.jsx
@@ -12,12 +12,13 @@ export const webSystemPrompt =
   "The system will then append the web search results at the end of the user message, starting with the token <|web_search_results|>. " +
   "Take note that the user CANNOT ACCESS the content under <|web_search_results|> - YOU should incorporate these results into your response. " +
   "Additionally, If the token <|web_search_results|> is present in the user's message, then you MUST NOT request another web search.\n\n" +
-  "IMPORTANT! You should choose to search the web ONLY if ANY of the following circumstances are met: " +
+  "IMPORTANT! You should choose to search the web ONLY if ANY of the following circumstances are met:\n" +
   "1. User is asking about current events or something that requires real-time information (news, sports scores, 'latest' information, etc.)\n" +
   "2. User is asking about some term you are unfamiliar with (e.g. if it's a little-known term, if it's a person you don't know well, etc.)\n" +
   "3. User is asking about anything that involves numerical facts (e.g. distances between places, population count, sizes, date and time, etc.)\n" +
   "4. User explicitly asks you to search or provide links to references.\n" +
-  "If the user's query does NOT require a web search, YOU MUST NEVER run one for no reason.\n\n" +
+  "If the user's query does NOT require a web search, YOU MUST NEVER run one for no reason.\n" +
+  "However, NEVER refuse to search the web if the user asks you to provide such information.\n\n" +
   "When using web search results, you are encouraged to cite references where appropriate, using inline links in standard markdown format.\n" +
   "You are also allowed to make use of web search results from previous messages, if they are STRONGLY RELEVANT to the current message.\n\n" +
   "When you are not running a web search, respond completely as per normal - there is NO NEED to mention that you're not searching. \n" +
@@ -33,7 +34,7 @@ export const getWebResult = async (query) => {
     let data = {
       api_key: APIKey,
       query: query,
-      max_results: 5,
+      max_results: 4,
       search_depth: "advanced",
     };
 
