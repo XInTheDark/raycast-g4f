715de704917ba7d582f61cd43624bb270abe5244
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-07-25 22:13:17 +0800

    rename file

--- src/configureLocalAPI.jsx
+++ src/configureLocalAPI.jsx
@@ -6,7 +6,7 @@ import { Form, ActionPanel, Action, useNavigation, showToast, Toast } from "@ray
 import { useState, useEffect } from "react";
 import { getOllamaModelsComponent } from "./api/Providers/ollama_local";
 
-export default function ConfigureLocalApi() {
+export default function ConfigureLocalAPI() {
   const [executablePath, setExecutablePath] = useState("");
   const [timeout, setTimeout] = useState("");
   const [g4fModelsComponent, setG4fModelsComponent] = useState([]);
