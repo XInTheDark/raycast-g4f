d1b2cb4a9504fec74812326a3ea3e5c993acd280
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-11-02 17:36:33 +0800

    Add AI4Chat (gpt-4) and DarkAI (gpt-4o) provider (#142)
    The second provider was quite difficult to get right, since they send
    incomplete chunks sometimes...

---
+++ src/api/Providers/ai4chat.js
@@ -0,0 +1,45 @@
+import fetch from "node-fetch";
+
+import { format_chat_to_prompt } from "../../classes/message.js";
+
+const headers = {
+  accept: "*/*",
+  "accept-language": "en-US,en;q=0.9",
+  "cache-control": "no-cache",
+  "content-type": "application/json",
+  origin: "https://www.ai4chat.co",
+  pragma: "no-cache",
+  priority: "u=1, i",
+  referer: "https://www.ai4chat.co/gpt/talkdirtytome",
+  "sec-ch-ua": '"Chromium";v="129", "Not=A?Brand";v="8"',
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": '"Linux"',
+  "sec-fetch-dest": "empty",
+  "sec-fetch-mode": "cors",
+  "sec-fetch-site": "same-origin",
+  "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+};
+
+const api_url = "https://www.ai4chat.co/generate-response";
+
+export const AI4ChatProvider = {
+  name: "Ai4Chat",
+  generate: async function (chat) {
+    const payload = {
+      messages: [{ role: "user", content: format_chat_to_prompt(chat) }],
+    };
+
+    let response = await fetch(api_url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(payload),
+    });
+
+    const result = await response.text();
+    const json_result = JSON.parse(result);
+    const message = json_result.message || "";
+    const clean_message = message.replace(/<[^>]+>/g, "");
+
+    return clean_message;
+  },
+};
---
+++ src/api/Providers/darkai.js
@@ -0,0 +1,76 @@
+import fetch from "node-fetch";
+
+import { format_chat_to_prompt } from "../../classes/message.js";
+
+const headers = {
+  accept: "text/event-stream",
+  "content-type": "application/json",
+  origin: "https://www.aiuncensored.info",
+  referer: "https://www.aiuncensored.info/",
+  "user-agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+};
+
+const api_url = "https://darkai.foundation/chat";
+
+export const DarkAIProvider = {
+  name: "DarkAI",
+  generate: async function* (chat, options) {
+    chat.unshift({
+      role: "system",
+      content:
+        "You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture. You are now being connected with a human.",
+    });
+    const prompt = format_chat_to_prompt(chat);
+
+    const data = {
+      query: prompt,
+      model: options.model,
+    };
+
+    const response = await fetch(api_url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    });
+
+    const reader = response.body;
+
+    let prevChunks = "";
+
+    for await (let str of reader) {
+      let lines = str.toString().replace(/\r/g, "\n").split("\n");
+      for (let line of lines) {
+        line = line.trim();
+        if (!line) continue;
+
+        const isData = line.startsWith("data: ");
+        if (isData || prevChunks) {
+          if (!isData) {
+            // Incomplete chunk, we try to append it to the previous one and process it
+            line = prevChunks + line;
+          }
+
+          try {
+            let data = JSON.parse(line.slice(6));
+            if (data["event"] === "text-chunk") {
+              let chunk = data["data"]["text"];
+              if (chunk) {
+                yield chunk;
+              }
+            } else if (data["event"] === "stream-end") {
+              return;
+            }
+
+            prevChunks = ""; // reset
+          } catch {
+            // either the chunk begins with "data: ", or it's a part of an incomplete chunk
+            // in both cases the following assignment will build the incomplete chunk
+            // (since we already appended the line to prevChunks if it's the second case)
+            prevChunks = line;
+          }
+        }
+      }
+    }
+  },
+};
