b5f8b5661d8cd455a2a0ecd1f63308903073256c
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-11-18 21:45:12 +0800

    more error handling

--- src/helpers/customCommands.jsx
+++ src/helpers/customCommands.jsx
@@ -38,73 +38,82 @@ export class CustomCommand {
     const main = parts[0].trim();
     let processed;
 
-    switch (main) {
-      case "input" || "selection":
-        processed = this.input;
-        break;
-      case "clipboard":
-        processed = (await Clipboard.read()).text;
-        break;
-      case "date":
-        // e.g. 1 June 2022
-        processed = new Date().toLocaleDateString([], { year: "numeric", month: "long", day: "numeric" });
-        break;
-      case "time":
-        // e.g. 6:45 pm
-        processed = new Date().toLocaleTimeString([], { hour: "numeric", minute: "numeric" });
-        break;
-      case "datetime":
-        // e.g. 1 June 2022 at 6:45 pm
-        processed = `${new Date().toLocaleDateString([], {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })} at ${new Date().toLocaleTimeString([], { hour: "numeric", minute: "numeric" })}`;
-        break;
-      case "day":
-        // e.g. Monday
-        processed = new Date().toLocaleDateString([], { weekday: "long" });
-        break;
-      case "browser-tab":
-        processed = await getBrowserTab();
-        break;
-      case "shell": {
-        const toast = await showToast(Toast.Style.Animated, "Running shell command");
+    try {
+      switch (main) {
+        case "input" || "selection":
+          processed = this.input;
+          break;
+        case "clipboard":
+          processed = (await Clipboard.read()).text;
+          break;
+        case "date":
+          // e.g. 1 June 2022
+          processed = new Date().toLocaleDateString([], { year: "numeric", month: "long", day: "numeric" });
+          break;
+        case "time":
+          // e.g. 6:45 pm
+          processed = new Date().toLocaleTimeString([], { hour: "numeric", minute: "numeric" });
+          break;
+        case "datetime":
+          // e.g. 1 June 2022 at 6:45 pm
+          processed = `${new Date().toLocaleDateString([], {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })} at ${new Date().toLocaleTimeString([], { hour: "numeric", minute: "numeric" })}`;
+          break;
+        case "day":
+          // e.g. Monday
+          processed = new Date().toLocaleDateString([], { weekday: "long" });
+          break;
+        case "browser-tab":
+          processed = await getBrowserTab();
+          break;
+        case "shell": {
+          const toast = await showToast(Toast.Style.Animated, "Running shell command");
 
-        const command = parts.slice(1).join("|").trim();
-        processed = await execShellNoStream(command);
-        console.log(command + "\n" + processed);
+          const command = parts.slice(1).join("|").trim();
+          processed = await execShellNoStream(command);
+          console.log(command + "\n" + processed);
 
-        await toast.hide();
-        break;
+          await toast.hide();
+          break;
+        }
+        default:
+          processed = t;
+          break;
       }
-      default:
-        processed = t;
-        break;
+    } catch (e) {
+      console.log(e);
+      processed = t;
     }
 
     // modifiers are applied after the main processing, except for shell commands
     if (["shell"].includes(main)) return processed;
 
-    const modifiers = parts.slice(1).map((x) => x.trim());
-    for (const mod of modifiers) {
-      switch (mod) {
-        case "uppercase":
-          processed = processed.toUpperCase();
-          break;
-        case "lowercase":
-          processed = processed.toLowerCase();
-          break;
-        case "trim":
-          processed = processed.trim();
-          break;
-        case "percent-encode":
-          processed = encodeURIComponent(processed);
-          break;
-        case "json-stringify":
-          processed = JSON.stringify(processed);
-          break;
+    try {
+      const modifiers = parts.slice(1).map((x) => x.trim());
+      for (const mod of modifiers) {
+        switch (mod) {
+          case "uppercase":
+            processed = processed.toUpperCase();
+            break;
+          case "lowercase":
+            processed = processed.toLowerCase();
+            break;
+          case "trim":
+            processed = processed.trim();
+            break;
+          case "percent-encode":
+            processed = encodeURIComponent(processed);
+            break;
+          case "json-stringify":
+            processed = JSON.stringify(processed);
+            break;
+        }
       }
+    } catch (e) {
+      console.log(e);
     }
 
     return processed;
