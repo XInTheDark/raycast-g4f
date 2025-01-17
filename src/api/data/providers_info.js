cb8c746608efb645f7a970b94c24fb27dad2c002
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2025-01-17 20:13:13 +0800

    remove unused code

--- src/api/data/providers_info.js
+++ src/api/data/providers_info.js
@@ -15,9 +15,6 @@ import { ReplicateProvider } from "../Providers/replicate.js";
 import { PhindProvider } from "../Providers/phind.js";
 import { GeminiProvider } from "../Providers/google_gemini.js";
 
-/// Special or unused providers
-import { OpenAIProvider } from "../Providers/openai.js";
-
 export const providers = [
   NexraProvider,
   DeepInfraProvider,
@@ -34,7 +31,8 @@ export const providers = [
   GeminiProvider,
 ];
 
-// Initialise providers_info
+// Initialise all providers info
+// { provider internal name, {provider object, model, stream, extra options} }
 const providers_info = {};
 for (let provider of providers) {
   if (providers.enabled === false) continue;
@@ -44,70 +42,6 @@ for (let provider of providers) {
   }
 }
 export { providers_info };
-
-/// All providers info
-// { provider internal name, {provider object, model, stream, extra options} }
-// prettier-ignore
-export const providers_info_unused = {
-  NexraChatGPT: { provider: NexraProvider, model: "chatgpt", stream: true },
-  NexraGPT4o: { provider: NexraProvider, model: "gpt-4o", stream: true },
-  NexraGPT4: { provider: NexraProvider, model: "gpt-4-32k", stream: false },
-  NexraBing: { provider: NexraProvider, model: "Bing", stream: true },
-  NexraLlama31: { provider: NexraProvider, model: "llama-3.1", stream: true },
-  NexraGeminiPro: { provider: NexraProvider, model: "gemini-pro", stream: true },
-  DeepInfraLlama33_70B: { provider: DeepInfraProvider, model: "meta-llama/Llama-3.3-70B-Instruct-Turbo", stream: true },
-  DeepInfraLlama32_90B_Vision: { provider: DeepInfraProvider, model: "meta-llama/Llama-3.2-90B-Vision-Instruct", stream: true, context_tokens: 8000 },
-  DeepInfraLlama32_11B_Vision: { provider: DeepInfraProvider, model: "meta-llama/Llama-3.2-11B-Vision-Instruct", stream: true },
-  DeepInfraLlama31_70B: { provider: DeepInfraProvider, model: "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo", stream: true },
-  DeepInfraLlama31_8B: { provider: DeepInfraProvider, model: "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo", stream: true },
-  DeepInfraLlama31_405B: { provider: DeepInfraProvider, model: "meta-llama/Meta-Llama-3.1-405B-Instruct", stream: true },
-  DeepInfraQwen25_72B: { provider: DeepInfraProvider, model: "Qwen/Qwen2.5-72B-Instruct", stream: true },
-  DeepInfraQwen25Coder_32B: { provider: DeepInfraProvider, model: "Qwen/Qwen2.5-Coder-32B-Instruct", stream: true },
-  DeepInfraQwQ_32B_Preview: { provider: DeepInfraProvider, model: "Qwen/QwQ-32B-Preview", stream: true },
-  DeepInfraWizardLM2_8x22B: { provider: DeepInfraProvider, model: "microsoft/WizardLM-2-8x22B", stream: true },
-  DeepInfraLlama31Nemotron70B: { provider: DeepInfraProvider, model: "nvidia/Llama-3.1-Nemotron-70B-Instruct", stream: true },
-  DeepInfraDeepSeek25: { provider: DeepInfraProvider, model: "deepseek-ai/DeepSeek-V2.5", stream: true },
-  Blackbox: { provider: BlackboxProvider, model: "blackbox", stream: true },
-  BlackboxLlama31_405B: { provider: BlackboxProvider, model: "llama-3.1-405b", stream: true },
-  BlackboxLlama31_70B: { provider: BlackboxProvider, model: "llama-3.1-70b", stream: true },
-  BlackboxLlama33_70B: { provider: BlackboxProvider, model: "llama-3.3-70b", stream: true },
-  BlackboxGemini15Flash: { provider: BlackboxProvider, model: "gemini-1.5-flash", stream: true },
-  BlackboxQwQ32BPreview: { provider: BlackboxProvider, model: "qwq-32b-preview", stream: true },
-  BlackboxGPT4o: { provider: BlackboxProvider, model: "gpt-4o", stream: true },
-  BlackboxClaude35Sonnet: { provider: BlackboxProvider, model: "claude-3.5-sonnet", stream: true },
-  BlackboxGeminiPro: { provider: BlackboxProvider, model: "gemini-pro", stream: true },
-  DuckDuckGo_GPT4oMini: { provider: DuckDuckGoProvider, model: "gpt-4o-mini", stream: true, context_tokens: 4096 },
-  DuckDuckGo_Claude3Haiku: { provider: DuckDuckGoProvider, model: "claude-3-haiku-20240307", stream: true, context_tokens: 4096 },
-  DuckDuckGo_Llama31_70B: { provider: DuckDuckGoProvider, model: "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo", stream: true, context_tokens: 4096 },
-  DuckDuckGo_Mixtral_8x7B: { provider: DuckDuckGoProvider, model: "mistralai/Mixtral-8x7B-Instruct-v0.1", stream: true, context_tokens: 4096 },
-  BestIM_GPT4oMini: { provider: BestIMProvider, model: "", stream: true },
-  // RocksClaude35Sonnet: { provider: RocksProvider, model: "claude-3-5-sonnet-20241022", stream: true },
-  // RocksClaude3Opus: { provider: RocksProvider, model: "claude-3-opus-20240229", stream: true },
-  // RocksChatGPT4oLatest: { provider: RocksProvider, model: "chatgpt-4o-latest", stream: true },
-  // RocksGPT4o: { provider: RocksProvider, model: "gpt-4o", stream: true },
-  // RocksGPT4: { provider: RocksProvider, model: "gpt-4", stream: true },
-  // RocksWizardLM2_8x22B: { provider: RocksProvider, model: "WizardLM-2-8x22B", stream: true },
-  // RocksLlama31_405B: { provider: RocksProvider, model: "llama-3.1-405b-turbo", stream: true },
-  // RocksLlama31_70B: { provider: RocksProvider, model: "llama-3.1-70b-turbo", stream: true },
-  // ChatgptFree: { provider: ChatgptFreeProvider, model: "", stream: true },
-  AI4Chat: { provider: AI4ChatProvider, model: "", stream: false, context_tokens: 4096 },
-  DarkAI: { provider: DarkAIProvider, model: "gpt-4o", stream: true },
-  Mhystical: { provider: MhysticalProvider, model: "gpt-4", stream: false },
-  PizzaGPT: { provider: PizzaGPTProvider, model: "", stream: false },
-  MetaAI: { provider: MetaAIProvider, model: "", stream: true },
-  ReplicateLlama3_8B: { provider: ReplicateProvider, model: "meta/meta-llama-3-8b-instruct", stream: true },
-  ReplicateLlama3_70B: { provider: ReplicateProvider, model: "meta/meta-llama-3-70b-instruct", stream: true },
-  ReplicateLlama31_405B: { provider: ReplicateProvider, model: "meta/meta-llama-3.1-405b-instruct", stream: true },
-  ReplicateMixtral_8x7B: { provider: ReplicateProvider, model: "mistralai/mixtral-8x7b-instruct-v0.1", stream: true },
-  Phind: { provider: PhindProvider, model: "", stream: true },
-  GoogleGemini: { provider: GeminiProvider, model: ["gemini-1.5-pro-002", "gemini-1.5-flash-002", "gemini-1.5-flash-latest"], stream: true },
-  GoogleGeminiFlash: { provider: GeminiProvider, model: ["gemini-2.0-flash-exp", "gemini-1.5-flash-002"], stream: true },
-  GoogleGeminiExperimental: { provider: GeminiProvider, model: ["gemini-exp-1206", "gemini-2.0-flash-exp", "gemini-2.0-flash-thinking-exp-1219"], stream: true },
-  GoogleGeminiThinking: { provider: GeminiProvider, model: ["gemini-2.0-flash-thinking-exp-1219"], stream: true },
-
-  // Providers unused in raycast-g4f
-  OpenAI_API: { provider: OpenAIProvider, model: "gpt-4o", stream: true, api_key: null },
-};
 
 /// Providers that support file uploads
 export const file_supported_providers = [GeminiProvider, DeepInfraProvider];
